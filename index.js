ad17bb8cff067c4235d3f4d082fd872d607dec2d
glaucopv <glauco_02@hotmail.com>
2024-02-20 23:28:42 -0400

    arquivo index js criado express e nodemon adicionados como dependencias e scripts start/dev criados

---
+++ index.js
@@ -0,0 +1,13 @@
+const { prototype } = require('events');
+const express = require('express');
+const app = express();
+
+const port = 3000
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World')
+});
+
+app.listen(port, () => {
+    console.info(`App rodando em http://localhost:${port}`);
+});
